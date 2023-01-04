8ec9ea5a693e93341a10a208e727a9b54335aca1
skumarscse <skumarscse@gmail.com>
2023-01-04 21:42:16 +0530

    course-website-4

---
+++ Course-Website/js/index.js
@@ -0,0 +1,4 @@
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+let scrollTopBtn = document.getElementById("scroll-top");
